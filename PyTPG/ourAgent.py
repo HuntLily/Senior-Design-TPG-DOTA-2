853c50b8bd2f95f5d493d4c3ebf74f9f6449115f
Sean Hasse <seanmhasse57@gmail.com>
2020-05-10 01:50:57 -0500

    reverted changes
    I didn't like the changes to the FF and games played so I reverted them.

--- PyTPG/ourAgent.py
+++ PyTPG/ourAgent.py
@@ -40,7 +40,7 @@ Arguments for TPG setup.
 # population size
 parser.add_option("-P", "--pop", type="int", dest="popSize", default=200)
 parser.add_option("-g", "--gens", type="int", dest="gens", default=100)
-parser.add_option("-r", "--gameReps", type="int", dest="gameReps", default=10)
+parser.add_option("-r", "--gameReps", type="int", dest="gameReps", default=1)
 
 (opts, args) = parser.parse_args()
 
@@ -58,7 +58,7 @@ class ServerHandler(BaseHTTPRequestHandler):
     def getFitness(self, state, win, deaths, kills):
         print("comment 7")
         # last hits, denies, net worth, difference in tower hp%, difference in hero hp%, level diff, time, win
-        return 10*state[24] + 15*state[25] + (1/4)*state[23] + (state[58]/state[59] - state[64]/state[65]) * 200 + \
+        return 10*state[24] + 15*state[25] + state[23] + (state[58]/state[59] - state[64]/state[65]) * 200 + \
                (state[2]/state[3] - state[32]/state[33]) * 500 + (state[1]-state[31])*100 + (1/(state[56]-1200)) * \
                1000 + 2000*win + 500*kills + (500 - (250 * deaths))
 
